ecc807d37c65398f75d87640f6d9054758d01550
Juliano Sales <julianosaless@users.noreply.github.com>
2018-02-17 00:52:52 -0200

    Update definition type
    Add new parameter  optional domain  to method setCookie

--- Abp/Framework/scripts/abp.d.ts
+++ Abp/Framework/scripts/abp.d.ts
@@ -361,9 +361,9 @@
         * @param {string} key
         * @param {string} value 
         * @param {Date} expireDate (optional). If not specified the cookie will expire at the end of session.
-        * @param {string} path (optional)
+        * @param {string} domain (optional)
         */
-        function setCookieValue(key: string, value: string, expireDate?: Date, path?: string): void;
+        function setCookieValue(key: string, value: string, expireDate?: Date, path?: string, domain?: string): void;
 
         /**
         * Gets a cookie with given key.
